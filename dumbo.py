f07780b34570ccc95928fe184f360a4d8838b35c
Theo <theo.godin@student.umons.ac.be>
2023-04-15 15:00:19 +0200

    added file reader from input

--- dumbo.py
+++ dumbo.py
@@ -1,1 +1,19 @@
-print("hello world !")
+import sys
+import ply.lex as lex
+
+def read_file(file):
+    lines = ""
+    with open(file, 'r') as f:
+        lines = [line.strip() for line in f.readlines()]
+    return lines
+
+def init_variables():
+    pass
+
+def inject_variables():
+    pass
+
+if __name__ == '__main__':
+    arg1 = sys.argv[1]
+    arg2 = sys.argv[2]
+    print(read_file(arg1))
